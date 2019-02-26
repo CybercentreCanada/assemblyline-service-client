fb4b093683f54b64c32da35e4e13eb6af78a7a61
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-02-26 15:04:30 -0500

    Mock forge for get_classification

---
+++ common/mock_forge.py
@@ -0,0 +1,8 @@
+from svc_client import Client
+
+svc_client = Client("http://0.0.0.0:5000")
+
+
+def get_classification():
+    from assemblyline.common.classification import Classification
+    return Classification(svc_client.help.get_classification_definition())
