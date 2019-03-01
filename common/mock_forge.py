0bba4cc78e521f25dac265be5b4b5a7e1ffdeea5
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-03-01 08:53:11 -0500

    Added mock get_constants

--- common/mock_forge.py
+++ common/mock_forge.py
@@ -6,3 +6,7 @@ svc_client = Client("http://0.0.0.0:5000")
 def get_classification():
     from assemblyline.common.classification import Classification
     return Classification(svc_client.help.get_classification_definition())
+
+
+def get_constants():
+    return svc_client.help.get_systems_constants()
