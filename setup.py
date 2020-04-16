2f0a5b1289de849c03500cbc2659b0f5269070d9
Steve Garon <steve.garon@cyber.gc.ca>
2020-04-16 16:09:46 -0400

    Make sure setup.py reads it's long description from the README.md file

--- setup.py
+++ setup.py
@@ -11,13 +11,17 @@ elif 'BUILD_SOURCEBRANCH' in os.environ:
     full_tag_prefix = 'refs/tags/v'
     package_version = os.environ['BUILD_SOURCEBRANCH'][len(full_tag_prefix):]
 
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="assemblyline-service-client",
     version=package_version,
-    description="Assemblyline (v4) automated malware analysis framework - Service client components.",
-    long_description="This package provides the service client components for "
-                     "the Assemblyline v4 malware analysis framework.",
+    description="Assemblyline 4 - Service client",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/CybercentreCanada/assemblyline-service-client/",
     author="CCCS Assemblyline development team",
     author_email="assemblyline@cyber.gc.ca",
@@ -30,7 +34,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    keywords="assemblyline malware gc canada cse-cst cse cst cyber cccs",
+    keywords="assemblyline automated malware analysis gc canada cse-cst cse cst cyber cccs",
     packages=find_packages(exclude=['test/*']),
     install_requires=[
         'assemblyline',
