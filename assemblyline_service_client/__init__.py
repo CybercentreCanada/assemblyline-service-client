87e89f4047690e0542f5792ee99fe1db4f7c4bd1
Adam Douglass <adam.douglass@cyber.gc.ca>
2020-04-29 11:21:07 -0400

    Keep version data in the package itself in a static file

--- assemblyline_service_client/__init__.py
+++ assemblyline_service_client/__init__.py
@@ -14,6 +14,11 @@ import requests
 from assemblyline.common import log
 
 __all__ = ['Client', 'ClientError']
+__version__ = "4.0.0.dev0"
+_package_version_path = os.path.join(os.path.dirname(__file__), 'VERSION')
+if os.path.exists(_package_version_path):
+    with open(_package_version_path) as _package_version_file:
+        __version__ = _package_version_file.read().strip()
 
 MAX_RETRY_BACKOFF = 10
 SUPPORTED_API = 'v1'
